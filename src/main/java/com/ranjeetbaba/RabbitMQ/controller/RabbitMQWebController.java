e5d41b248d0d98f79a18598d6dd3c3162aaaf601
Ranjeet Kumar Mishra <ranjeet.mishra@delhivery.com>
2019-01-26 16:22:57 +0530

    first commmit

---
+++ src/main/java/com/ranjeetbaba/RabbitMQ/controller/RabbitMQWebController.java
@@ -0,0 +1,29 @@
+package com.ranjeetbaba.RabbitMQ.controller;
+
+import com.ranjeetbaba.RabbitMQ.model.Employee;
+import com.ranjeetbaba.RabbitMQ.service.RabbitMQSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/ranjeetbaba-rabbitmq/")
+public class RabbitMQWebController {
+
+    @Autowired
+    RabbitMQSender rabbitMQSender;
+
+    @GetMapping(value = "/producer")
+    public String producer(@RequestParam("empName") String empName, @RequestParam("empId") String empId) {
+
+        Employee emp=new Employee();
+        emp.setEmpId(empId);
+        emp.setEmpName(empName);
+        rabbitMQSender.send(emp);
+
+        return "Message sent to the RabbitMQ RanjeetBaba Successfully";
+    }
+
+}
---
+++ src/main/java/com/ranjeetbaba/RabbitMQ/service/RabbitMQSender.java
@@ -0,0 +1,27 @@
+package com.ranjeetbaba.RabbitMQ.service;
+
+
+import com.ranjeetbaba.RabbitMQ.model.Employee;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQSender {
+
+    @Autowired
+    private AmqpTemplate rabbitTemplate;
+
+    @Value("${ranjeetbaba.rabbitmq.exchange}")
+    private String exchange;
+
+    @Value("${ranjeetbaba.rabbitmq.routingkey}")
+    private String routingkey;
+
+    public void send(Employee company) {
+        rabbitTemplate.convertAndSend(exchange, routingkey, company);
+        System.out.println("Send msg = " + company);
+
+    }
+}
